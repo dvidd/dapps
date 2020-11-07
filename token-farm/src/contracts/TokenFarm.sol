01dae48a3599124e8042484d77c58c30f9b5e3d4
David L Barrero <davidlopezbarrero@gmail.com>
2020-11-07 17:10:37 +0100

    Tokenfarm

---
+++ token-farm/src/contracts/TokenFarm.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.5.16;
+
+import "./DappToken.sol";
+import "./DaiToken.sol";
+
+
+contract TokenFarm {
+    string public name = "Dapp Token Farm";
+
+    constructor() {
+        
+    }
+}
