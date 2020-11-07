81e3c34b04c81573cf1d50451fd04ef99d2e47a0
David L Barrero <davidlopezbarrero@gmail.com>
2020-11-07 22:12:47 +0100

    Tests.js

---
+++ token-farm/test/TokenFarm.test.js
@@ -0,0 +1,33 @@
+const DappToken = artifacts.require("DappToken");
+const DaiToken = artifacts.require("DaiToken");
+const TokenFarm = artifacts.require("TokenFarm");
+
+require("chai")
+  .use(require("chai-as-promised"))
+  .should();
+
+function tokens(n) {
+  return web3.utils.toWei(n, "Ether");
+}
+
+contract("TokenFarm", accounts => {
+  // Write here tests
+  let daiToken, dappToken, tokenFarm;
+  before(async () => {
+    // Load contracts
+    daiToken = await DaiToken.new();
+    dappToken = await DappToken.new();
+    tokenFarm = await TokenFarm.new(dappToken.address, daiToken.address);
+
+    // Transfer all dapp token to farm
+    await dappToken.transfer(tokenFarm.address, tokens("1000000"));
+  });
+
+  describe("Mock DAI deployment", async () => {
+    it("has a name", async () => {
+      let daiToken = await DaiToken.new();
+      const name = await daiToken.name();
+      assert.equal(name, "Mock DAI Token");
+    });
+  });
+});
