ddc073afdb7152880c1878fb74430588d010d862
David L Barrero <davidlopezbarrero@gmail.com>
2020-11-07 23:04:12 +0100

    TokenFarm.sol

--- token-farm/src/contracts/TokenFarm.sol
+++ token-farm/src/contracts/TokenFarm.sol
@@ -3,16 +3,23 @@ pragma solidity >=0.5.16;
 import "./DappToken.sol";
 import "./DaiToken.sol";
 
-
 contract TokenFarm {
     string public name = "Dapp Token Farm";
     DappToken public dappToken;
     DaiToken public daiToken;
 
-
     constructor(DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
         daiToken = _daiToken;
-
     }
+
+    // Stakes Tokens into the smart contract (Deposit)
+    function stakeTokens(uint256 _amount) public {
+        // Transfar mock dai to this contract for stakin
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+    }
+
+    // Unstaking Tokens (Withdraw)
+
+    // Issuing Tokens
 }
