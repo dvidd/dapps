5fd1062e7209d23c1cec7971bbeee9d702646f01
David L Barrero <davidlopezbarrero@gmail.com>
2020-11-08 16:57:26 +0100

    Finish tokenFarm contract

--- token-farm/src/contracts/TokenFarm.sol
+++ token-farm/src/contracts/TokenFarm.sol
@@ -7,6 +7,7 @@ contract TokenFarm {
     string public name = "Dapp Token Farm";
     DappToken public dappToken;
     DaiToken public daiToken;
+    address public owner;
 
     address[] public stakers;
     mapping(address => uint256) public stakingBalance;
@@ -16,10 +17,12 @@ contract TokenFarm {
     constructor(DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
         daiToken = _daiToken;
+        owner = msg.sender;
     }
 
     // Stakes Tokens into the smart contract (Deposit)
     function stakeTokens(uint256 _amount) public {
+        require(_amount > 0, "amount cannot be 0");
         // Transfar mock dai to this contract for stakin
         daiToken.transferFrom(msg.sender, address(this), _amount);
         // Update stakeBalance
@@ -34,6 +37,30 @@ contract TokenFarm {
     }
 
     // Unstaking Tokens (Withdraw)
+    function unstakeTokens() public {
+        // Fetch staking balance
+        uint256 balance = stakingBalance[msg.sender];
+        // requiere amount greater
+        require(balance > 0, "staking balance cannot be 0");
+        // Tranfer mock Dai tokens to this contract for staking
+        daiToken.transfer(msg.sender, balance);
+        // Reset staking balance
+        stakingBalance[msg.sender] = 0;
+        // Updaye staking balance
+        isStaking[msg.sender] = false;
+    }
 
     // Issuing Tokens
+
+    function issueTokens() public {
+        require(msg.sender == owner, "caller must be the owner");
+        // Issuin token
+        for (uint256 i = 0; i < stakers.length; i++) {
+            address recipient = stakers[i];
+            uint256 balance = stakingBalance[recipient];
+            if (balance > 0) {
+                dappToken.transfer(recipient, balance);
+            }
+        }
+    }
 }
