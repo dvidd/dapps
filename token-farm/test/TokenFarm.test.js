0c31ab40f4c22392bd079a1859f1bda6a0c15612
David L Barrero <davidlopezbarrero@gmail.com>
2020-11-08 16:57:46 +0100

    Tests for token farm

--- token-farm/test/TokenFarm.test.js
+++ token-farm/test/TokenFarm.test.js
@@ -54,7 +54,7 @@ contract("TokenFarm", ([owner, investor]) => {
   });
 
   describe("Farming tokens", async () => {
-    it("rewards investor for staking mDai tokens", async () => {
+    it("rewards investors for staking mDai tokens", async () => {
       let result;
       // Check balance
       result = await daiToken.balanceOf(investor);
@@ -67,8 +67,79 @@ contract("TokenFarm", ([owner, investor]) => {
       await daiToken.approve(tokenFarm.address, tokens("100"), {
         from: investor
       });
-
       await tokenFarm.stakeTokens(tokens("100"), { from: investor });
+
+      // check staking result
+      result = await daiToken.balanceOf(investor);
+      assert.equal(
+        result.toString(),
+        tokens("0"),
+        "investor Mock DAI wallet balance correct after staking"
+      );
+
+      result = await daiToken.balanceOf(tokenFarm.address);
+      assert.equal(
+        result.toString(),
+        tokens("100"),
+        "Token Farm Mock DAI balance correct after staking"
+      );
+
+      result = await tokenFarm.stakingBalance(investor);
+      assert.equal(
+        result.toString(),
+        tokens("100"),
+        "investor staking balance correct after staking"
+      );
+      result = await tokenFarm.isStaking(investor);
+      assert.equal(
+        result.toString(),
+        "true",
+        "investor staking status correct after staking"
+      );
+
+      // Issuing tokens
+      await tokenFarm.issueTokens({ from: owner });
+      // check balances after issuance
+      result = await dappToken.balanceOf(investor);
+      assert.equal(
+        result.toString(),
+        tokens("100"),
+        "investor Dapp Token wallet balance correct after issuance"
+      );
+      // Ensure that only owner can issue tokens
+      await tokenFarm.issueTokens({ from: investor }).should.be.rejected;
+
+      // Unstake the tokens
+      await tokenFarm.unstakeTokens({ from: investor });
+
+      // check result after unstaking
+
+      result = await daiToken.balanceOf(investor);
+      assert.equal(
+        result.toString(),
+        tokens("100"),
+        "investor Mock DAI wallet balance correct after staking"
+      );
+      result = await daiToken.balanceOf(tokenFarm.address);
+      assert.equal(
+        result.toString(),
+        tokens("0"),
+        "Token Farm Mock DAI balance correct after staking"
+      );
+
+      result = await tokenFarm.stakingBalance(investor);
+      assert.equal(
+        result.toString(),
+        tokens("0"),
+        "investor staking balance correct after staking"
+      );
+
+      result = await tokenFarm.isStaking(investor);
+      assert.equal(
+        result.toString(),
+        "false",
+        "investor staking staking status correct after staking"
+      );
     });
   });
 });
