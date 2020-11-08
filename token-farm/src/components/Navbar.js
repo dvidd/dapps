154f7c7fe0202dbcb6c7b4c6f8caa41091a238d9
David L Barrero <davidlopezbarrero@gmail.com>
2020-11-08 17:12:56 +0100

    Migration new contracts

--- token-farm/src/components/Navbar.js
+++ token-farm/src/components/Navbar.js
@@ -1,19 +1,16 @@
-import React, { Component } from 'react'
-import farmer from '../farmer.png'
+import React, { Component } from "react";
+import farmer from "../farmer.png";
 
 class Navbar extends Component {
-
   render() {
     return (
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+      <nav className="navbar light-dark fixed-top bg-light flex-md-nowrap p-1 shadow">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="http://www.dappuniversity.com/bootcamp"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img src={farmer} width="30" height="30" className="d-inline-block align-top" alt="" />
-          &nbsp; DApp Token Farm
+          🧑🏻‍🌾 &nbsp; <b>Token Farm</b>
         </a>
 
         <ul className="navbar-nav px-3">
